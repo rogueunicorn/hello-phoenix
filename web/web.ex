52c7f42b1f058c61b97bcb923ed3691308072bf0
Toby Hede <tobyhede@info-architects.net>
2016-01-15 08:07:44 +1100

    Upgrade to phoenix 1.1

--- web/web.ex
+++ web/web.ex
@@ -18,8 +18,9 @@ defmodule HelloPhoenix.Web do
 
   def model do
     quote do
-      use Ecto.Model
+      use Ecto.Schema
 
+      import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
     end
@@ -30,7 +31,7 @@ defmodule HelloPhoenix.Web do
       use Phoenix.Controller
 
       alias HelloPhoenix.Repo
-      import Ecto.Model
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
 
       import HelloPhoenix.Router.Helpers
@@ -62,7 +63,7 @@ defmodule HelloPhoenix.Web do
       use Phoenix.Channel
 
       alias HelloPhoenix.Repo
-      import Ecto.Model
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
     end
   end
